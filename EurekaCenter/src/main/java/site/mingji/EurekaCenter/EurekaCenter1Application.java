4f4ae28404696a354add6ccaf001acbd8870c87d
翟佳元 <543566378@qq.com>
2023-03-30 16:41:57 +0800

    变更

--- EurekaCenter/src/main/java/site/mingji/EurekaCenter/EurekaCenter1Application.java
+++ EurekaCenter/src/main/java/site/mingji/EurekaCenter/EurekaCenter1Application.java
@@ -3,26 +3,27 @@ package site.mingji.EurekaCenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 //测试冲突合并解决
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaCenter1Application {
 
-	public static void main(String[] args) {
-		System.out.println("补丁测试");
-		//分支合并测试
-		//测试可否临时修改合并结果
-		
-		//后来发现推送不了，很乱
+    public static void main(String[] args) {
+        System.out.println("补丁测试");
+        //分支合并测试
+        //测试可否临时修改合并结果
+
+        //后来发现推送不了，很乱
 
 //再试试
-		//
-		SpringApplication.run(EurekaCenter1Application.class, args);
-		//本地修改
+        //
+        SpringApplication.run(EurekaCenter1Application.class, args);
+        //本地修改
 
 //测试merge
-		//重置
-	}
-	
-	
+        //重置
+    }
+
+
 }
