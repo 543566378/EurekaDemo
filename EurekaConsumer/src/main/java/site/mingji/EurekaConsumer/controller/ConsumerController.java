2df1d7caa260eba860d02fad8aaf2ce00aea47e1
翟佳元 <543566378@qq.com>
2023-04-06 11:39:52 +0800

    看FeignConfiguration2的第11行和TestBean的第10行，为啥一个执行，一个没执行

--- EurekaConsumer/src/main/java/site/mingji/EurekaConsumer/controller/ConsumerController.java
+++ EurekaConsumer/src/main/java/site/mingji/EurekaConsumer/controller/ConsumerController.java
@@ -1,19 +1,22 @@
 package site.mingji.EurekaConsumer.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import site.mingji.EurekaConsumer.server.TestServer;
 
 @RestController
 public class ConsumerController {
 
-	@Autowired
-	TestServer server;
-	
+    @Autowired
+    TestServer server;
+
+//    @Autowired
+//    TestBean getBean;
+
     @GetMapping("/rpc")
     public String rpc() {
-    	return server.test();
+        return server.test();
     }
 }
--- EurekaConsumer/src/main/java/site/mingji/EurekaConsumer/server/TestServer.java
+++ EurekaConsumer/src/main/java/site/mingji/EurekaConsumer/server/TestServer.java
@@ -1,15 +1,15 @@
 package site.mingji.EurekaConsumer.server;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import site.mingji.EurekaConsumer.config.FeignConfiguration1;
+import site.mingji.EurekaConsumer.config.FeignConfiguration2;
 
-@Component
-@FeignClient("Eureka-Provider1")
+@FeignClient(value = "Eureka-Provider1", configuration = FeignConfiguration2.class)
 public interface TestServer {
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test();
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    String test();
 
 }
